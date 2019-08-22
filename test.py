279a604f6c2a82736870ca2a8fbfceecc3dd340f
sarashs <arash.sheikh65@gmail.com>
2019-08-22 14:41:25 -0700

    Qubo_toy problem is now tested
    The essential file is updated accordingly. It contains all of the class/functions that we have written so far.
    
    Test.py is a new file which tests the simulated annealing parameters optimization.

---
+++ test.py
@@ -0,0 +1,9 @@
+from essentials import *
+
+a=SA(3, params={'weight_objective': [1, 0, 2, 0], 'weight_end': [1, 0, 2, 0],
+                 'weight_start': [1, 0, 2, 0] ,'weight_others': [1, 0, 2, 0],
+                 'weight_and': [6, 4, 15, 0],
+                 'chain_strength': [7, 3, 12, 0], 'annealing_time': [99, 99, 99, 1]},
+                 T=1, T_min=0.00001, alpha=0.8, max_iter=5)
+
+a.anneal()
